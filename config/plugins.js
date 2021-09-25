b60510fd60246ce4e81bc5119f3b4c7a4b2d77e5
Christian <chrbudtz@gmail.com>
2021-09-25 10:44:23 +0200

    updated backend

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    breakpoints: {
+      xlarge: 1920 ,
+      large: 1024,
+      medium: 800,
+      small: 640,
+    }
+  }
+  // ...
+});
